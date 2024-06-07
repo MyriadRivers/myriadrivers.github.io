562a7fb34f39f67d825c65aa2c1c13eded14e2cf
Jason Gao <jasongao678@gmail.com>
2024-06-06 22:58:12 -0400

    Update resume and dates

--- src/pages/projects/SpotifyKaraoke/SpotifyKaraoke.tsx
+++ src/pages/projects/SpotifyKaraoke/SpotifyKaraoke.tsx
@@ -29,7 +29,7 @@ function SpotifyKaraoke() {
         <div className={"projectContents"} ref={contentsRef}>
             <ProjectTitle
                 text={"Spotify Karaoke"}
-                subtitle={"Nov–Feb 2024"}
+                subtitle={"Aug–Dec 2023"}
                 links={[
                     { text: "demo", url: "https://jasoncgao.com/spotify-karaoke/" },
                     { text: "front-end GitHub", url: "https://github.com/MyriadRivers/spotify-karaoke" },
